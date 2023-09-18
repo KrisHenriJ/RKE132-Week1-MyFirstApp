82d13053f408ad3e23cd84323ee351b1ef1959fb
Kris Henri Järvi <kris@Kriss-MacBook-Air.local>
2023-09-19 01:25:50 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// JOU MEES
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name");
+//string-sõne
+
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + " !"); //output
+    //string interpolation
+
+Console.WriteLine($"Hello {userName} !"); //output
